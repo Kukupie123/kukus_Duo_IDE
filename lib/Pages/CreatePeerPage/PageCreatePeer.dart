00263c74d2d1ab5c70cc700d72dadec54fde148b
kuchukpie@gmail.com <kuchukboromd@gmail.com>
2023-04-16 10:49:42 +0530

    Completed Create Offer and Join offer

--- lib/Pages/CreatePeerPage/PageCreatePeer.dart
+++ lib/Pages/CreatePeerPage/PageCreatePeer.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, sized_box_for_whitespace
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'package:kukus_multi_user_ide/Backend/provider/ProviderBackend.dart';
 import 'package:provider/provider.dart';
 
@@ -13,14 +16,25 @@ class PageCreatePeer extends StatefulWidget {
 
 class _PageCreatePeerState extends State<PageCreatePeer> {
   var offerGeneratedTC = TextEditingController();
+  var remoteSDPTC = TextEditingController();
+  var candidateTC = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     ProviderBackend providerBackend =
         Provider.of<ProviderBackend>(context, listen: false);
+    providerBackend.webRTCServices.peerConnection?.onIceConnectionState =
+        (state) {
+      if (state == RTCIceConnectionState.RTCIceConnectionStateConnected ||
+          state == RTCIceConnectionState.RTCIceConnectionStateConnected) {
+        statusSC.add("Successfully Connected");
+      }
+    };
     pasteOfferToTextField(providerBackend);
   }
+
+  StreamController<String> statusSC = StreamController();
 
   @override
   Widget build(BuildContext context) {
@@ -31,43 +45,70 @@ class _PageCreatePeerState extends State<PageCreatePeer> {
       body: SizedBox(
         width: double.infinity,
         height: double.infinity,
-        child: Column(
-          children: [
-            TextField(
-              controller: offerGeneratedTC,
-              decoration:
-                  InputDecoration(hintText: "Generating Offer Please wait...."),
-            ),
-            Text(
-                "Copy the Generated Code above and paste it as the Remote Description in the Client that is trying to join"),
-            TextField(
-              controller: offerGeneratedTC,
-              decoration: InputDecoration(
-                  hintText:
-                      "Paste Generated Answer of the Client trying to join...."),
-            ),
-            TextButton(
-                onPressed: () {}, child: Text("Confirm Generated Answer")),
-            TextField(
-              controller: offerGeneratedTC,
-              decoration: InputDecoration(
-                  hintText:
-                      "Paste Copied Candidate of the Client trying to join...."),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text("Confirm Candidate"),
-            ),
-          ],
-        ),
+        child: StreamBuilder<String>(
+            stream: statusSC.stream,
+            builder: (context, snapshot) {
+              String? data = snapshot.data;
+              return Column(
+                children: [
+                  Text(data == null ? "Idle" : snapshot.data!),
+                  TextField(
+                    controller: offerGeneratedTC,
+                    decoration: InputDecoration(
+                        hintText: "Generating Offer Please wait...."),
+                  ),
+                  Text(
+                      "Copy the Generated Code above and paste it as the Remote Description in the Client that is trying to join"),
+                  TextField(
+                    controller: remoteSDPTC,
+                    decoration: InputDecoration(
+                        hintText:
+                            "Paste Generated Answer of the Client trying to join...."),
+                  ),
+                  TextButton(
+                      onPressed: () async {
+                        if (remoteSDPTC.text.isEmpty) {
+                          statusSC.add("Textfield is empty");
+                          return;
+                        }
+                        statusSC.add("Setting Remote SDP");
+                        await providerBackend.webRTCServices
+                            .setRemoteSDP(remoteSDPTC.text);
+                        statusSC.add("Setting Remote SDP Complete.");
+                      },
+                      child: Text("Confirm Generated Answer")),
+                  TextField(
+                    controller: candidateTC,
+                    decoration: InputDecoration(
+                        hintText:
+                            "Paste Copied Candidate of the Client trying to join...."),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      if (candidateTC.text.isEmpty) {
+                        statusSC.add("Textfield is empty");
+                        return;
+                      }
+                      statusSC.add("Adding Candidate");
+                      await providerBackend.webRTCServices
+                          .addCandidate(candidateTC.text);
+                      statusSC.add("Adding Candidate Complete.");
+                    },
+                    child: Text("Confirm Candidate"),
+                  ),
+                ],
+              );
+            }),
       ),
     );
   }
 
   void pasteOfferToTextField(ProviderBackend providerBackend) async {
+    statusSC.add("Loading Offer");
     String? offerSDP = await providerBackend.webRTCServices.createOffer();
     setState(() {
       offerGeneratedTC.text = offerSDP!;
     });
+    statusSC.add("Generated Offer");
   }
 }

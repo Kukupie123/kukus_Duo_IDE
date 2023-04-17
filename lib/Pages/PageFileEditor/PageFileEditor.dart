e80204e161ad6b74bc4e3c372fedc1b43414de55
kuchukpie@gmail.com <kuchukboromd@gmail.com>
2023-04-17 17:53:36 +0530

    Added file editor page

---
+++ lib/Pages/PageFileEditor/PageFileEditor.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PageFileEditor extends StatefulWidget {
+  const PageFileEditor({Key? key}) : super(key: key);
+
+  @override
+  State<PageFileEditor> createState() => _PageFileEditorState();
+}
+
+class _PageFileEditorState extends State<PageFileEditor> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
